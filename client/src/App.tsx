d450c5ec65c416d265ec88adba9934d8f2ba4e15
BigBoyPro <BigBoyPro@proton.me>
2024-04-22 22:56:35 +0200

    quelques changements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
-
+import StoryLogo from "./assets/story-logo.svg?react";
 function App() {
 
   return (
-    <>
-    </>
+      <>
+          <div className={"main-page"}>
+              <div className={"play-box"}>
+                  <h1>hello world</h1>
+                  <StoryLogo width={200} height={200}/>
+              </div>
+          </div>
+          <footer>i'm foot</footer>
+      </>
   )
 }
 
