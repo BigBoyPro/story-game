be658f3899a185afab2315476e9725feb9e15960
BigBoyPro <BigBoyPro@proton.me>
2024-04-23 14:40:55 +0200

    quelques changements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
 import StoryLogo from "./assets/story-logo.svg?react";
+import React, {useState} from "react";
 function App() {
-
+    const [nickname, setNickname] = useState('');
+    const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        // You can use the nickname state here
+        console.log(nickname);
+    }
   return (
       <>
           <div className={"main-page"}>
               <div className={"play-box"}>
-                  <h1>hello world</h1>
-                  <StoryLogo width={200} height={200}/>
+                  <StoryLogo width="200" height="200"/>
+                  <h1>Story Mode</h1>
+                  <form onSubmit={(event) => {handleSubmit(event)}} >
+                      <input onChange={(event) => setNickname(event.target.value)}
+                          type="text" placeholder="Nickname"/>
+                      <button type="submit" >Play</button>
+                  </form>
               </div>
           </div>
           <footer>i'm foot</footer>
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,3 +5,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/api/baby', (req, res) => {
+    res.json({baby: '👶'});
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on http://localhost:4000');
+});
