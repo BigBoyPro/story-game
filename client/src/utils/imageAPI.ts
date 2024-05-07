687a44d5185ff3461391c8075d92765c3207c605
Lahmar <radhia.lahmar@etu.univ_amu.fr>
2024-05-07 13:27:51 +0200

    imageAPI

--- client/src/utils/imageAPI.ts
+++ client/src/utils/imageAPI.ts
@@ -1,10 +1,19 @@
-const IMGUR_CLIENT_ID = '14c652722afe251'; // Replace this with your actual Client ID from Imgur
+import cloudinary from 'cloudinary';
+
+// Configure Cloudinary
+cloudinary.v2.config({
+  cloud_name: 'dduoumojc',
+  api_key: '556213698218321',
+  api_secret: '4ctIVBdEtSuxytKILPwFggp_NxA'
+});
+
 
 export const uploadImage = async (imageFile: File): Promise<string | null> => {
     const formData = new FormData();
     formData.append('image', imageFile);
+    formData.append('upload_preset', 'ml_default');
 
-    try {
+    /*try {
         const response = await fetch('https://api.imgur.com/3/image', {
             method: 'POST',
             headers: {
@@ -25,5 +34,22 @@ export const uploadImage = async (imageFile: File): Promise<string | null> => {
     } catch (error) {
         console.error('Error uploading image:', error);
         return null;
+    }*/
+    try {
+      const response = await fetch('https://api.cloudinary.com/v1_1/dduoumojc/image/upload', {
+        method: 'POST',
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to upload image to Cloudinary');
+      }
+  
+      const data = await response.json();
+      return data.secure_url; // This is the URL of the uploaded image
+    } catch (error) {
+      console.error('Failed to upload image to Cloudinary:', error);
+      return null;
     }
+  
 };
