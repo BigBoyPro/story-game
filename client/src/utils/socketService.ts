8a2f75ec7560877008fe7e1aeca174ed5ab98565
BigBoyPro <BigBoyPro@proton.me>
2024-06-02 13:53:20 +0200

    client port 80 and host

--- client/src/utils/socketService.ts
+++ client/src/utils/socketService.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import {Lobby, OpError, Story, StoryElement} from "../../../shared/sharedTypes.ts";
 
 
-const socket = io('https://story-game-serv.vercel.app:1234');
+const socket = io('https://story-game-server.adaptable.app:80');
 
 export const userId = localStorage.getItem('userId')
     || (() => {
