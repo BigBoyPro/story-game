6458f22a00e80ff0bc9fdfcad027b7421cf4f299
BigBoyPro <BigBoyPro@proton.me>
2024-06-02 15:56:39 +0200

    little fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -39,7 +39,7 @@ try {
         }
     });
 
-    resetGames(io, pool).then(r => {
+    resetGames(io, pool).then(() => {
         setupSocketHandlers(io, pool);
         server.listen(PORT, () => {
             console.log("Server is running on port " + PORT);
