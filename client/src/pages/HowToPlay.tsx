471d51ac038f973809d53f7de98a75612834b1f3
BOUTAHRI <tarek.boutahri@etu.univ-amu.fr>
2024-06-06 22:01:18 +0200

    ajout du bouton retour

--- client/src/pages/HowToPlay.tsx
+++ client/src/pages/HowToPlay.tsx
@@ -1,3 +1,4 @@
+/*
 // src/pages/HowToPlay.tsx
 
 import './HowToPlay.css';
@@ -5,6 +6,29 @@ import './HowToPlay.css';
 function HowToPlay() {
     return (
         <div className="how-to-play-page">
+            <div className="rules-text">
+                <h1>Rules of the Game</h1>
+                <h2>Here are the rules of the game...</h2>
+                <p> ici on doit écrire les règle du jeu.</p>
+            </div>
+        </div>
+    );
+}
+
+
+export default HowToPlay;*/
+
+// src/pages/HowToPlay.tsx
+
+import { Link } from 'react-router-dom';
+import './HowToPlay.css';
+
+function HowToPlay() {
+    return (
+        <div className="how-to-play-page">
+            <Link to="/" className="back-button">
+                <img src="../../public/BackButtonBack1.png" alt="Back Button" />
+            </Link>
             <div className="rules-text">
                 <h1>Rules of the Game</h1>
                 <h2>Here are the rules of the game...</h2>
