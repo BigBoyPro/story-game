d7dc96b000d4ae9a709b0a6d48cb3a6a70aadb21
Laeticia HAKEM <laeticia.hakem@etu.univ-amu.fr>
2024-06-07 21:12:30 +0200

    chat box !!

--- client/src/components/StoryElementComponent/StoryElementComponent.tsx
+++ client/src/components/StoryElementComponent/StoryElementComponent.tsx
@@ -162,6 +162,7 @@ const StoryElementComponent = forwardRef(
                 return <div/>
             case StoryElementType.Text:
                     const textArea = <textarea value={element.content}
+                                               className="chat-bubble"
                                                onChange={(e) => handleContentChange(e.target.value)}
                                                disabled={!isEditable}/>;
                     if (isEditable) {
