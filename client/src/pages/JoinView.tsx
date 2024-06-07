03c83fe92924a7e2bd5486f8a7b98eb61b86a9cd
BigBoyPro <BigBoyPro@proton.me>
2024-06-07 21:02:18 +0200

    Revert "responsive join"
    This reverts commit 36e69a55c907f2f80f4090aa3ed6de7a50cd537c.

--- client/src/pages/JoinView.tsx
+++ client/src/pages/JoinView.tsx
@@ -1,8 +1,6 @@
 import React, {useContext, useEffect, useState} from "react";
 import {requestCreateLobby, requestJoinLobby} from "../utils/socketService.ts";
-import StoryGameLogo from "../assets/logo.svg?react";
-import playButtonIcon from "../assets/playButton.png";
-import helpButtonIcon from "../assets/helpButton.png";
+import logo from '../assets/logo.svg';
 import {useNavigate} from "react-router-dom";
 import {LobbyContext} from "../LobbyContext.tsx";
 import {Page, redirection} from "../App.tsx";
@@ -19,7 +17,7 @@ function JoinView() {
     const [nickname, setNickname] = useState('');
     const [lobbyCode, setLobbyCode] = useState('');
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (event : React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         redirection(lobby, navigate, Page.Join);
 
@@ -39,32 +37,30 @@ function JoinView() {
     };
 
     return (
-        <>
-            <video autoPlay loop muted
-                   className={"background"}>
-                <source src="../JoinView.mp4" type="video/mp4" />
-            </video>
-            <header>
-                <button type="button" className={"button-3d-icon button-3d-icon--help"} onClick={handleHelpClick}>
-                    <img src={helpButtonIcon} alt="Help Button"/>
-                </button>
-            </header>
-            <div className={"main-page"}>
+      <>
+          <button type="button" className={"button-3d-icon-help"} onClick={handleHelpClick}></button>
+          {/*style={{ position: "absolute", width: "100%", height: "100%", objectFit: "cover",zIndex: 0}}*/}
+          <video autoPlay loop muted
+                 className={"background"}>
+              <source src="../JoinView.mp4" type="video/mp4" />
+          </video>
+              <div className={"main-page"}>
+                  <div className={"main-box"}>
+                      {<img src={logo} alt="Logo" width={450} height={450} />}
+                      <form onSubmit={(event) => handleSubmit(event)}>
 
-                <div className={"main-box"}>
-                    <form onSubmit={(event) => handleSubmit(event)}>
-                        <StoryGameLogo className={"join__logo"} />
-                        <input onChange={(event) => setNickname(event.target.value)}
-                               type="text" placeholder="Nickname" className="join__input"/>
-                        <input onChange={(event) => setLobbyCode(event.target.value)}
-                               type="text" placeholder="Optional Lobby Code" className="join__input"/>
-                        <button type="submit" className={"button-3d-icon button-3d-icon--play"}>
-                            <img src={playButtonIcon} alt="Play Button"/>
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </>
+                          <input onChange={(event) => setNickname(event.target.value)}
+                                 type="text" placeholder="Nickname" className="input-placeholder"/>
+
+                          <input onChange={(event) => setLobbyCode(event.target.value)}
+                                 type="text" placeholder="Optional Lobby Code" className="input-placeholder"/>
+
+
+                          <button type="submit" className={"button-3d-icon-play"}></button>
+                      </form>
+                  </div>
+              </div>
+      </>
     );
 }
 
