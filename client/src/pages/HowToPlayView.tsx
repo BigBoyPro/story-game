cd028c15b3541f49141d38242df9859ed9a49f68
BOUTAHRI <tarek.boutahri@etu.univ-amu.fr>
2024-06-11 23:20:09 +0200

    petit changemlent

--- client/src/pages/HowToPlayView.tsx
+++ client/src/pages/HowToPlayView.tsx
@@ -31,7 +31,7 @@ function HowToPlayView() {
 
     return (
         <div className="how-to-play-page">
-            <button className="button" onClick={goPrevRule}>Previous</button> {/* Move the left arrow button here */}
+            <button className="button-HTP" onClick={goPrevRule}>Previous</button> {/* Move the left arrow button here */}
             <div className="rules-text">
                 <Link to="/" className="back-button">
                     <img src={backButtonIcon} alt="Back Button" />
@@ -42,7 +42,7 @@ function HowToPlayView() {
                     {rules[currentRule]}
                 </p>
             </div>
-            <button className="button" onClick={goNextRule}>Next</button> {/* Move the right arrow button here */}
+            <button className="button-HTP" onClick={goNextRule}>Next</button> {/* Move the right arrow button here */}
         </div>
     );
 }
